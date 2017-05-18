625a775427d281ff4f50a8c2d9dd4571b016b1d9
e7d <michael@e7d.io>
2017-05-18 16:31:22 +0200

    updated build script

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
-vagrant plugin install vagrant-vbguest
+vagrant_plugin_install() {
+	plugins=$(vagrant plugin list | grep $1)
+	if [[ -z $plugins ]]; then
+		vagrant plugin install $1
+	else
+		vagrant plugin update $1
+	fi
+}
 
-vagrant up --provision
-vagrant vbguest
+echo "Configure Vagrant"
+vagrant_plugin_install vagrant-vbguest
 
-rm ps.box
+echo "Build box"
+vagrant destroy --force
+vagrant up --provider=virtualbox --install-provider --destroy-on-error
+
+echo "Package box"
+rm -rf ps.box
 vagrant package --output ps.box
